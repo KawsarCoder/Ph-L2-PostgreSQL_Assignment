5293bee077f2dd48325b861e40d7c14fa192591e
KawsarCoder <md.kawsar.developer@gmail.com>
2025-05-26 19:52:53 +0600

    problem one done

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -13,3 +13,45 @@ VALUES
 
 SELECT * FROM rangers;
 
+CREATE TYPE species_status as ENUM ('Endangered', 'Vulnerable');
+
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(255) NOT NULL,
+    scientific_name VARCHAR(255) NOT NULL,
+    discovery_date DATE,
+    conservation_status species_status NOT NULL DEFAULT 'Endangered'
+);
+
+INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status)
+VALUES
+('Snow Leopard', 'Panthera uncia','1775-01-01', 'Endangered'),
+('Bengal Tiger','Panthera tigris tigris','1758-01-01','Endangered'),
+('Red Panda','Ailurus fulgens','1825-01-01','Vulnerable'),
+('Asiatic Elephant','Elephas maximus indicus','1758-01-01','Endangered');
+
+SELECT * FROM species;
+
+CREATE TABLE sightings(
+    sighting_id SERIAL PRIMARY KEY,
+    ranger_id INT REFERENCES rangers(ranger_id),
+    species_id INT REFERENCES species(species_id),
+    location VARCHAR(255),
+    sighting_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    notes VARCHAR(255)
+);
+
+
+INSERT INTO sightings (ranger_id, species_id, location, sighting_time, notes)
+VALUES
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
+
+DROP TABLE sightings;
+SELECT * FROM sightings;
+
+INSERT INTO rangers (name, region)
+VALUES
+('Derek Fox', 'Coastal PlainsCoastal Plains');
